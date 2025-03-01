b068b9179339907c71dd618d56823a990d506db1
soeun-727 <soeun2215562@sm.ac.kr>
2025-03-01 18:11:25 +0900

    8주차_웹프로젝트_25.5기_박소은

---
+++ 8주차_웹프로젝트_25.5기_박소은/review3_process.php
@@ -0,0 +1,23 @@
+<?php
+$conn = mysqli_connect(
+  'localhost',
+  'root',
+  '1',
+  'review');
+$sql = "
+  INSERT INTO three
+    (id, review, created)
+    VALUES(
+        '{$_POST['id']}',
+        '{$_POST['review']}',
+        NOW()
+    )
+";
+$result = mysqli_query($conn, $sql);
+if($result === false){
+  echo '리뷰를 등록하지 못했습니다. 관리자에게 문의해주세요';
+  error_log(mysqli_error($conn));
+} else {
+  echo '성공적으로 리뷰를 등록했습니다. <a href="review3.php">돌아가기</a>';
+}
+?>
